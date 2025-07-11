4db798833387d8bc4f555aa4fa8a2a6dd1ce0d7c
Bruno Winter <brunohdwinter@gmail.com>
2025-07-11 10:59:34 -0400

    feat: Criando o Service/Repository/Controller/Factory/Seeder do veiculo

---
+++ app/Services/VeiculoService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\VeiculoRepository;
+
+class VeiculoService
+{
+    protected $repository;
+
+    public function __construct(VeiculoRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function index()
+    {
+        return $this->repository->all();
+    }
+
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    public function store(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function destroy($id)
+    {
+        return $this->repository->delete($id);
+    }
+}
