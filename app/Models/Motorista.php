57744fad1d30b44c0f790484f3c7bb8358336244
Bruno Winter <brunohdwinter@gmail.com>
2025-07-11 13:44:53 -0400

    feat: finalizando model de Motorista

--- app/Models/Motorista.php
+++ app/Models/Motorista.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Motorista extends Model
 {
-    use HasFactory;
+    use SoftDeletes, HasFactory;
+
+    protected $table = 'motoristas';
+
+    protected $fillable = [
+        'nome',
+        'data_nascimento',
+        'cnh',
+    ];
+
+    protected $casts = [
+        'data_nascimento' => 'date',
+    ];
 }
