525a324faf9123bdd8eda1e56a1c3d4aa7d43ad5
Bruno Winter <brunohdwinter@gmail.com>
2025-07-13 04:16:43 -0400

    feat: criando sidebar com navegacoes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,19 +2,22 @@ import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import AppLayout from './Layouts/AppLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue');
+        const page = (await pages[`./Pages/${name}.vue`]()).default;
+
+        page.layout = page.layout || AppLayout;
+
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
