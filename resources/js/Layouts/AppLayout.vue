6592502cc6ae730c97add36c5d741fd3cb86b8fa
Bruno Winter <brunohdwinter@gmail.com>
2025-07-13 04:16:43 -0400

    feat: criando sidebar com navegacoes

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex min-h-screen bg-gray-100">
+    <SideBar />
+    <main class="flex-1 p-6">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import SideBar from '@/Components/SideBar.vue';
+
+</script>
