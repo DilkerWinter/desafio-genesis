144bc1cfb9e005ac58ef26f6796e3ed6988125ad
Bruno Winter <brunohdwinter@gmail.com>
2025-07-13 18:39:54 -0400

    remove: deletando template do starter kit do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,12 @@
 <?php
 
 use App\Http\Controllers\MotoristaController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VeiculoController;
 use App\Http\Controllers\ViagemController;
-use App\Models\Veiculo;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [ViagemController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 //Receber json de todos os Veiculos/Motoristas
 Route::get('/motoristas/list', [MotoristaController::class, 'list'])->name('motoristas.list');
@@ -37,5 +17,3 @@ Route::resource('veiculos', VeiculoController::class);
 Route::resource('motoristas', MotoristaController::class);
 Route::resource('viagens', ViagemController::class);
 
-
-require __DIR__.'/auth.php';
